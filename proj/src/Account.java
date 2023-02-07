85f57ac6d3d35a738ebb06d208930abd24955cf0
Kriaeyl <osireikri@gmail.com>
2023-02-08 02:31:36 +0800

    Baseline + csv

---
+++ proj/src/Account.java
@@ -0,0 +1,46 @@
+public class Account {
+    private String accountNumber;
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private int pinNumber;
+    private double availableBalance;
+    private double totalBalance;
+    private double transferLimit;
+    private boolean isAuthenticated;
+    private boolean isAdmin;
+    
+    public Account(String accountNumber, String username, String password, String firstName, String lastName, int pinNumber,
+			double availableBalance, double totalBalance, double transferLimit, boolean isAuthenticated, boolean isAdmin) {
+		super();
+		this.accountNumber = accountNumber;
+		this.username = username;
+        this.password = password;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.pinNumber = pinNumber;
+		this.availableBalance = availableBalance;
+		this.totalBalance = totalBalance;
+        this.transferLimit = transferLimit;
+        this.isAuthenticated = isAuthenticated;
+        this.isAdmin = isAdmin;
+	}
+
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+}
