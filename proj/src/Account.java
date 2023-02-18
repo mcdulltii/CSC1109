b6b36173273eb3c54544397692317b2c4cdf2c65
Kriaeyl <osireikri@gmail.com>
2023-02-18 20:49:29 +0800

    baseline revert??

--- proj/src/Account.java
+++ proj/src/Account.java
@@ -1,30 +1,35 @@
-public class Account extends Settings {
+public class Account {
     private String accountNumber;
-    // private int pinNumber;
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private int pinNumber;
     private double availableBalance;
     private double totalBalance;
     private double transferLimit;
     private boolean isAuthenticated;
     private boolean isAdmin;
-	private Authenticate auth;
 
-	public Account(String username) {
-		super(username);
-		auth = new Authenticate();
-		this.accountNumber = auth.generateAccountNumber();
-        this.isAuthenticated = false;
-        this.isAdmin = false;
-	}
-
-    public Account(String username, boolean isAdmin) {
-		super(username);
-		auth = new Authenticate();
-		this.accountNumber = auth.generateAccountNumber();
-        this.isAuthenticated = false;
+	public Account(String accountNumber, String username, String password, String firstName, String lastName, int pinNumber,
+            double availableBalance, double totalBalance, double transferLimit, boolean isAuthenticated, boolean isAdmin) {
+        super();
+        this.accountNumber = accountNumber;
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.pinNumber = pinNumber;
+        this.availableBalance = availableBalance;
+        this.totalBalance = totalBalance;
+        this.transferLimit = transferLimit;
+        this.isAuthenticated = isAuthenticated;
         this.isAdmin = isAdmin;
-	}
+    }
 
 	public String getAccountNumber() {
 		return accountNumber;
 	}
+
+	//missing getters, oh well :/
 }
