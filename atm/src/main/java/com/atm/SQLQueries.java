646f4de0b2f6800e5c8e383b8ff98fcde6233929
Phileo Teo <Phileo0604@gmail.com>
2023-03-01 21:04:52 +0800

    Edited SQLQueries.java

--- atm/src/main/java/com/atm/SQLQueries.java
+++ atm/src/main/java/com/atm/SQLQueries.java
@@ -1,21 +1,24 @@
 package com.atm;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.sql.*;
+
 public class SQLQueries {
+    final String db_url="jdbc:mysql://localhost:3306/oopasgdb";
     public String transactionId;
 
     public void executeQueryTransactions(String accountNumber, Date transactionDate, String transactionDetails, String chqNumber, Date valueDate, Double withdrawal, Double deposit, Double balance){
-        final String db_url="jdbc:mysql://localhost:3306/oopasgdb";
-        Connection conn = null;
+        Connection conn = getConnection();
+        String getTransactionCount = "select transactionId from transactions order by transactionId desc limit 1";
+        String sql = "insert into transactions (transactionId, accountNumber, transactionDate, transactionDetails, chqNumber, valueDate, withdrawal, deposit, balance) values (?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
+        ResultSet transactionCountResult = executeQuery(getTransactionCount);
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            conn = DriverManager.getConnection(db_url, "testAdmin", "password1");
-            String getTransactionCount = "select transactionId from transactions order by transactionId desc limit 1";
-            Statement getTransactionCountStmt = conn.createStatement();
-            ResultSet transactionCountResult = getTransactionCountStmt.executeQuery(getTransactionCount);
-            while (transactionCountResult.next())
-                transactionId = Integer.toString(transactionCountResult.getInt(1) + 1);
-        } catch (Exception e){
+            if (transactionCountResult != null)
+                while (transactionCountResult.next())
+                    transactionId = Integer.toString(transactionCountResult.getInt(1) + 1);
+        } catch (Exception e) {
             System.out.println(e);
         }
 
@@ -23,24 +26,65 @@ public class SQLQueries {
         if (transactionId == null)
             transactionId = "0";
 
-        if (conn != null) {
-            try {
-                String sql = "insert into transactions (transactionId, accountNumber, transactionDate, transactionDetails, chqNumber, valueDate, withdrawal, deposit, balance) values (?, ?, ?, ?, ?, ?, ?, ?, ?)";
-                PreparedStatement preparedStmt = conn.prepareStatement(sql);
-                preparedStmt.setString(1, transactionId);
-                preparedStmt.setString(2, accountNumber);
-                preparedStmt.setDate(3, transactionDate);
-                preparedStmt.setString(4, transactionDetails);
-                preparedStmt.setString(5, chqNumber);
-                preparedStmt.setDate(6, valueDate);
-                preparedStmt.setDouble(7, withdrawal);
-                preparedStmt.setDouble(8, deposit);
-                preparedStmt.setDouble(9, balance);
-                preparedStmt.execute();
-                conn.close();
-            } catch (Exception e){
-                System.out.println(e);
-            }
+        try {
+            PreparedStatement preparedStmt = conn.prepareStatement(sql);
+            preparedStmt.setString(1, transactionId);
+            preparedStmt.setString(2, accountNumber);
+            preparedStmt.setDate(3, transactionDate);
+            preparedStmt.setString(4, transactionDetails);
+            preparedStmt.setString(5, chqNumber);
+            preparedStmt.setDate(6, valueDate);
+            preparedStmt.setDouble(7, withdrawal);
+            preparedStmt.setDouble(8, deposit);
+            preparedStmt.setDouble(9, balance);
+            preparedStmt.execute();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        closeConnection(conn);
+    }
+
+    public void importAccounts() throws FileNotFoundException{
+        String sql = "SELECT * FROM accounts;";
+        ResultSet sqlResult = executeQuery(sql);
+        if (sqlResult == null) {
+            // Import CSV
+
+        }
+    }
+
+    private ResultSet executeQuery(String query) {
+        ResultSet statementResult = null;
+        Connection conn = getConnection();
+
+        try {
+            Statement sqlStatement = conn.createStatement();
+            statementResult = sqlStatement.executeQuery(query);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        closeConnection(conn);
+        return statementResult;
+    }
+
+    private Connection getConnection() {
+        Connection conn = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection(db_url, "testAdmin", "password1");
+        } catch (Exception e){
+            System.out.println(e);
+        }
+        return conn;
+    }
+
+    private void closeConnection(Connection conn) {
+        try {
+            conn.close();
+        } catch (Exception e) {
+            return;
         }
     }
 }
