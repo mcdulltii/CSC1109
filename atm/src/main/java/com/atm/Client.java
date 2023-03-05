d44095dccdbf83840869b86731bf61b01ba3986e
Kriaeyl <osireikri@gmail.com>
2023-03-06 06:34:33 +0800

    Added Client-Server

---
+++ atm/src/main/java/com/atm/Client.java
@@ -0,0 +1,69 @@
+package com.atm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+    private Socket socket;
+    private PrintStream out;
+    private BufferedReader in;
+
+    public Client(String ip, int port) {
+        try {
+            this.socket = new Socket(ip, port);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public void startConnection() {
+        try {
+            out = new PrintStream(socket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendMessage(String msg) {
+        out.println(msg + "\n");
+        try {
+            String line = in.readLine();
+            while(line != null && line.length() > 0)
+            {
+                System.out.println(line);
+                line = in.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        out.flush();
+    }
+
+    public void close() {
+        try {
+            in.close();
+            out.close();
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        Client c = new Client("127.0.0.1", 7777);
+        c.startConnection();
+        System.out.println("Type anything to start..."); //NOTE: does not actually do anything but idk why it needs sth to be typed before the server prompts sooooo...?
+        while (true) {
+            String input = sc.nextLine();
+            if (input != null && input.length() > 0)
+                c.sendMessage(input);
+        }
+    } 
+}
---
+++ atm/src/main/java/com/atm/Server.java
@@ -0,0 +1,143 @@
+package com.atm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Server extends Thread {
+    private ServerSocket serverSocket;
+
+    public void start(int port) throws IOException {
+        serverSocket = new ServerSocket(port);
+        this.start();
+    }
+
+    public void stopServer() {
+        try {
+            serverSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        this.interrupt();
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                // Call accept() to receive the next connection
+                Socket socket = serverSocket.accept();
+
+                // Pass the socket to the RequestHandler thread for processing
+                ThreadClientHandler requestHandler = new ThreadClientHandler(socket);
+                requestHandler.start();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        /* 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter admin password: ");
+        String password = sc.next();
+        if (password == "admin") { // TODO: Authenticate
+            // TODO: List of Admin Actions (copy from App.java im lazy rn)
+            System.out.println("Welcome Admin!");
+        }
+        // switch case all that for admin actions
+        // one of actions is open server????
+        System.out.println("i open server uwu");
+        */
+        Server s = new Server();
+        try {
+            s.start(7777);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class ThreadClientHandler extends Thread {
+    private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+    private Boolean authenticated = false;
+    private String inputLine = "";
+
+    public ThreadClientHandler(Socket socket) {
+        this.clientSocket = socket;
+    }
+
+    public void run() {
+        try {
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            out = new PrintWriter(clientSocket.getOutputStream(), true);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+        //NOTE: ALL out.println MUST END WITH \n TO LET BUFFEREDREADER KNOW END-OF-INPUT
+        //VV IMPT I SPEND 5HRS ON THIS AND IM VERY SAD
+        
+        /* 
+        // Get client username and password
+        while (!authenticated) {
+            try {
+                out.println("Enter username: \n");
+                out.flush();
+                String username = in.readLine();
+                out.println("Enter password: \n");
+                out.flush();
+                String password = in.readLine();
+                //TODO: Authenticate user
+                authenticated = true;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        //Print Menu
+        out.print("Long list of stuff\n1.\n2.\n3.\nEnter input: ");
+        out.flush();
+        */
+        
+        //idk why the code above doesnt work sighs
+        
+        //Some sample codes, entering ono closes the server, typing anything else echoes itback
+        try {
+            inputLine = in.readLine();
+            while (true) {
+                if (inputLine.length() > 0) {
+                    if ("ono".equals(inputLine)) {
+                        out.println("Connection Terminated.\n");
+                        out.flush();
+                        break;
+                    }
+
+                    out.println("From Server: " + inputLine);
+                    out.println("");
+                    out.flush();
+                }
+                inputLine = in.readLine();
+            }
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
+        try {
+            in.close();
+            out.close();
+            clientSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
